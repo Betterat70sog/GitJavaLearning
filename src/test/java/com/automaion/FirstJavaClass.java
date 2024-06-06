75b2a93ce6bd353256ce46246d3245581fc7ac37
betterat70sog <dauren.smagulov2147.ru@gmail.com>
2024-06-05 20:03:13 -0400

    refactored the code

--- src/test/java/com/automaion/FirstJavaClass.java
+++ src/test/java/com/automaion/FirstJavaClass.java
@@ -3,6 +3,7 @@ package com.automaion;
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("first line of code");
+        System.out.println("second line of code");
 
 
     }
